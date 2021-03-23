82aa00369327c2b5f6bf38a6e46b5107cb375655
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2021-03-22 21:04:07 -0700

    Added annotaions.

--- vision_lib.py
+++ vision_lib.py
@@ -7,12 +7,20 @@ from imutils.video.pivideostream import PiVideoStream
 import imutils
 import pprint 
 
-
+#Add the Google Cloud Vision credentials to the environemnt.
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'GCVision.json'
+
+#Declare the client object. 
 client = vision.ImageAnnotatorClient()
 object_to_id #leave empty for now 
 
+
 def get_image_from_frame(cap):
+    """
+    Each frame will return a png file saved as 'frame'. 
+    This file will be overwritten with each cycle.
+    The code below is similar to the livestream app we built in Term 2.
+    """
     ret, frame = cap.read()
     file = 'frame.png'
     cv2.imwrite(file,frame)
@@ -20,10 +28,12 @@ def get_image_from_frame(cap):
     return file
 
 def start_camera():
-
+    """
+    Start the camera. Make sure the Raspberry Pi recognizes the Picamera as the main interface for CV2.
+    """
     os.system('sudo modprobe bcm2835-v4l2') 
-
-    cap = cv2.VideoCapture(0)
+    
+    cap = cv2.VideoCapture(0) # Use the CV2 library's video capture function. 0 is the index of the main device camera.
     print("Starting camera")
 
     while True:
@@ -36,9 +46,4 @@ def start_camera():
     
     cap.release()
     cv2.destroyAllWindows()
-    
-    
-
-
-
-print("vision library launched successfully")
+   
