d7d353b9a6fe8768391c936674153a15a36738c2
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2021-03-22 21:13:05 -0700

    Added comments

--- object_recognizer.py
+++ object_recognizer.py
@@ -1,4 +1,8 @@
-#This Package is for Team Edge Term 3 
+"""
+object_recognizer.p authored by Google Code Next
+This program uses the Google Cloud Vision AI API to return the object that has the highest confidence score based on a camera input frame (.png)
+Run the code in Thonny on the Raspberry Pi with a Camera module attached.
+"""
 
 import os, io
 from google.cloud import vision
@@ -7,7 +11,7 @@ from time import sleep
 import pprint 
 
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'GCVision.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'GCVision.json' #the credetials to talk to the API.
 client = vision.ImageAnnotatorClient()
 print("starting object recognizer...")
 
@@ -21,7 +25,8 @@ def get_object_from_cloud(image_path):
     image = vision.Image(content=content)
     response = client.label_detection(image=image) #full response from Google cloud vision
     label_annotations = response.label_annotations #array of objects labeled
-    description = label_annotations[0].description #get description of one object
+    #print(label_annotations)
+    description = label_annotations[0].description #get description of one object, the first being the one with the highest confidnce score.
     if description:
         return description
     else:
@@ -37,7 +42,7 @@ def get_image_from_frame(cap):
 
 def start_camera():
     global object_to_find
-    os.system('sudo modprobe bcm2835-v4l2') 
+    os.system('sudo modprobe bcm2835-v4l2') #Force the Raspberry Pi to use the the Picamera, which CV2 will need to capture each frame.
 
     cap = cv2.VideoCapture(0)
     print("Starting camera")
@@ -53,7 +58,7 @@ def start_camera():
         if key == ord('q'): #press q to quit
             break
     
-    cap.release()
+    cap.release() #release the object when the app quits.
     cv2.destroyAllWindows()
 
 
